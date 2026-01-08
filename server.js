17f3c57f473ce30cdb36f66dbb0f8f9f5c205264
merndev9-lang <merndev9@gmail.com>
2026-01-08 19:48:16 +0530

    add cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://assesment-portal-digicoders.vercel.app/",
+  origin: "https://assesment-portal-digicoders.vercel.app",
   credentials: true
 }));app.use(express.urlencoded({ extended: true }));
 
